946337ad0ca83838f834ce07956117b716cbeb87
nate-mal <natanaelmalita@gmail.com>
2022-06-10 21:07:30 +0300

    add serverModal

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# calibri
+test
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import ServerModal from "./Components/ServerModal";
+
+function App() {
+  return (
+    <div className="App">
+      <ServerModal />
+    </div>
+  );
+}
+
+export default App;
